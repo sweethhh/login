255f5d2392486c34234ccbedde1ef9e49dae4476
Libra <491854473@qq.com>
2016-11-11 13:33:15 +0800

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-	var form=document.getElementsByTagName('form')[0];
-	var username=form.UserName[0];
-	var pass=form.pass[0];
-	var submit=form.log[0];
-	var reset=form.log[1];
-	
-};
